1aae21a0dd472ed75c1de543629e168ac28d08cd
Harsh Pathak <harsh9934530656pathak@gmail.com>
2025-09-13 11:41:17 +0530

    render 3

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,  // Allow all hosts
+    port: Number(process.env.PORT) || 3000,
+    allowedHosts: ['instagram-sales-agent-frontend.onrender.com']
+  }
 })
